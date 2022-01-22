ad77af2ec69da148706a30bf48c8965f4e44e37c
Rolando Garcia <rlnsanz@users.noreply.github.com>
2022-01-22 20:14:04 +0000

    intermediate push

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # kaggle-nlp-disasters
 https://www.kaggle.com/c/nlp-getting-started
+
+![tweet example](https://storage.googleapis.com/kaggle-media/competitions/tweet_screenshot.png)
+
+You can imagine how an NLP classifier would
+mistakenly label this tweet as one referring to a real 
+emergency, since it uses the word _ablaze_.
+
+## Competition Description
+Agencies and organizations will mine twitter,
+sometimes for emergency response.
+
+
+
+The Kaggle challenge is as follows:
+>Train an NLP classifier that can do well-enough at detecting real emergencies from a set of tweets.
+
+## Data
+* `sample_submission.csv`: Used for formatting your output in a way that Kaggle can score you
+* `train.csv`: Data you will use for training and validation
+* `test.csv`: Hold-out set
--- train.py
+++ train.py
@@ -3,7 +3,17 @@ import itertools
 import random
 
 import numpy as np
+import pandas as pd
 
+import flor
+
+flor.flags.NAME = "kaggle-nlp-disasters"
+flor.flags.REPLAY = False
+
+"""
+TRY SCIKIT LEARN FIRST
+WRITE THE README WITH INSTRUCTIONS, KAGGLE
+"""
 
 def get_data_batch():
     """
@@ -13,14 +23,13 @@ def get_data_batch():
     yield 1, {}
 
 
-def get_data():
-    return [batch for batch in get_data_batch()]
+def get_data(mode='pandas'):
+    if mode == 'pandas':
+        return pd.read_csv('data/train.csv')
+    else:
+        raise RuntimeError(f'Mode {mode} not implemented in function `get_data`')
+data = get_data()
 
-
-import flor
-
-flor.flags.NAME = "kaggle-nlp-disasters"
-flor.flags.REPLAY = False
 
 """
 The main loop of deep learning
@@ -33,6 +42,5 @@ for e in flor.it(range(100)):
         for s, batch in enumerate(get_data()):
             model[-1]["batch"] = batch
             print(s, batch)
-
     flor.SkipBlock.end(model)
     print(f"END OF EPOCH {e}")
