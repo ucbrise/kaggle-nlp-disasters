771bf11797d97f1d93692d6726bae4ec60ac226a
Rolando Garcia <rlnsanz@users.noreply.github.com>
2022-01-22 22:17:47 +0000

    Generating valid Kaggle output. Validation 69%

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
 
 import flor
 
@@ -15,19 +16,53 @@ flor.flags.REPLAY = False
 DEEP_LEARNING = False
 
 
-def get_data(mode="pandas"):
+def get_data(mode="pandas", test=False):
     if mode == "pandas":
-        return pd.read_csv("data/train.csv")
+        return pd.read_csv("data/train.csv" if not test else "data/test.csv")
     else:
         raise RuntimeError(f"Mode {mode} not implemented in function `get_data`")
 
 
 data = get_data()
-
 if not DEEP_LEARNING:
     # try scikit learn first
-    while flor.it(True):
+    def key_mapper(s: str, _=[]):
+        if s not in _:
+            k = len(_)
+            _.append(k)
+            return k + 1
+        else:
+            return _.index(s) + 1
 
+    integerMapped_keywords = (
+        data["keyword"].apply(lambda k: key_mapper(k) if not k is np.NaN else 0)
+    ).array.reshape(-1, 1)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        integerMapped_keywords, data["target"]
+    )
+    while flor.it(True):
+        # Naive first step: classify based on keyword
+        clf = GradientBoostingClassifier(
+            n_estimators=100,
+            learning_rate=1,
+            max_depth=3,
+            random_state=flor.pin("clf_seed", random.randint(0, 5000)),
+        ).fit(X_train, y_train)
+        score = clf.score(X_val, y_val)
+        print("Validation score: ", score)
+
+        test_data = get_data(test=True)
+        integerMapped_test = (
+            test_data["keyword"]
+            .apply(lambda k: key_mapper(k) if not k is np.NaN else 0)
+            .array.reshape(-1, 1)
+        )
+        preds = clf.predict(integerMapped_test)
+        # print("Test score: ", tscore)
+
+        preds_df = pd.DataFrame({"id": test_data["id"], "target": preds})
+        preds_df.to_csv("data/output.csv", index=False)
         break
 
 else:
@@ -44,3 +79,5 @@ else:
                 print(s, batch)
         flor.SkipBlock.end(model)
         print(f"END OF EPOCH {e}")
+
+print("FINISHED")
