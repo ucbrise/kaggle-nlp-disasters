33482677da116b7a6562d967cc517edf0fc46e0d
Alexis Wan <60990214+alexiswan99@users.noreply.github.com>
2022-01-28 06:21:52 +0000

    run

--- train_rnn.py
+++ train_rnn.py
@@ -10,13 +10,173 @@ import torch.optim as optim
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device
+
 label_field = Field(sequential=False, use_vocab=False, batch_first=True, dtype=torch.float)
 text_field = Field(tokenize='spacy', lower=True, include_lengths=True, batch_first=True)
 fields = [('words', text_field), ('target', label_field)]
 
-train, valid = TabularDataset.splits(path = 'data', train='train_rnn.csv', validation='valid_rnn.csv',format='CSV', fields=fields, skip_header=True)
+train, valid, test = TabularDataset.splits(path = 'data', train='train_rnn.csv', validation='valid_rnn.csv', test = 'test_rnn.csv', format='CSV', fields=fields, skip_header=True)
 
-train_iter = BucketIterator(train, batch_size=32, sort_key=lambda x: len(x.text), sort=True, sort_within_batch=True)
-valid_iter = BucketIterator(valid, batch_size=32, sort_key=lambda x: len(x.text), sort=True, sort_within_batch=True)
+train_iter = BucketIterator(train, batch_size=32, sort_key=lambda x: len(x.words), device = device, sort=True, sort_within_batch=True)
+valid_iter = BucketIterator(valid, batch_size=32, sort_key=lambda x: len(x.words), device = device, sort=True, sort_within_batch=True)
+test_iter = BucketIterator(test, batch_size=32, sort_key=lambda x: len(x.words), device = device, sort=True, sort_within_batch=True)
 
-text_field.build_vocab(train, min_freq=3)
+text_field.build_vocab(train, min_freq=5)
+
+# LSTM model 
+class LSTM(nn.Module):
+
+    def __init__(self, dimension=128):
+        super(LSTM, self).__init__()
+
+        self.embedding = nn.Embedding(len(text_field.vocab), 300)
+        self.dimension = dimension
+        self.lstm = nn.LSTM(input_size=300,
+                            hidden_size=dimension,
+                            num_layers=1,
+                            batch_first=True,
+                            bidirectional=True)
+        self.drop = nn.Dropout(p=0.5)
+
+        self.fc = nn.Linear(2*dimension, 1)
+
+    def forward(self, text, text_len):
+
+        text_emb = self.embedding(text)
+
+        packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
+        packed_output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
+
+        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
+        out_reverse = output[:, 0, self.dimension:]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+        text_fea = self.drop(out_reduced)
+
+        text_fea = self.fc(text_fea)
+        text_fea = torch.squeeze(text_fea, 1)
+        text_out = torch.sigmoid(text_fea)
+
+        return text_out
+
+# helper functions for check point 
+def save_checkpoint(save_path, model, optimizer, valid_loss):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimizer.state_dict(),
+                  'valid_loss': valid_loss}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+def load_checkpoint(load_path, model, optimizer):
+
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    model.load_state_dict(state_dict['model_state_dict'])
+    optimizer.load_state_dict(state_dict['optimizer_state_dict'])
+    
+    return state_dict['valid_loss']
+
+def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'train_loss_list': train_loss_list,
+                  'valid_loss_list': valid_loss_list,
+                  'global_steps_list': global_steps_list}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+def load_metrics(load_path):
+
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
+
+# training 
+def train(model, optimizer, criterion = nn.BCELoss(), train_loader = train_iter, valid_loader = valid_iter, num_epochs = 5, eval_every = len(train_iter) // 2, file_path = 'training_process', best_valid_loss = float("Inf")):  
+    running_loss = 0.0
+    valid_running_loss = 0.0
+    global_step = 0
+    train_loss_list = []
+    valid_loss_list = []
+    global_steps_list = []
+
+    # training loop
+    model.train()
+    for epoch in range(num_epochs):
+        for ((words, words_len), labels), _ in train_loader:           
+            labels = labels.to(device)
+            words = words.to(device)
+            words_len = words_len.to(device)
+            output = model(words, words_len)
+
+            loss = criterion(output, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # update running values
+            running_loss += loss.item()
+            global_step += 1
+
+            # evaluation step
+            if global_step % eval_every == 0:
+                model.eval()
+                with torch.no_grad():                    
+                  # validation loop
+                  for ((words, words_len), labels), _ in valid_loader:
+                      labels = labels.to(device)
+                      words = words.to(device)
+                      words_len = words_len.to(device)
+                      output = model(words, words_len)
+
+                      loss = criterion(output, labels)
+                      valid_running_loss += loss.item()
+
+                # evaluation
+                average_train_loss = running_loss / eval_every
+                average_valid_loss = valid_running_loss / len(valid_loader)
+                train_loss_list.append(average_train_loss)
+                valid_loss_list.append(average_valid_loss)
+                global_steps_list.append(global_step)
+
+                # resetting running values
+                running_loss = 0.0                
+                valid_running_loss = 0.0
+                model.train()
+
+                # print progress
+                print('Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, global_step, num_epochs*len(train_loader),
+                              average_train_loss, average_valid_loss))
+                
+                # checkpoint
+                #if best_valid_loss > average_valid_loss:
+                    #best_valid_loss = average_valid_loss
+                    #save_checkpoint(file_path + '/model.pt', model, optimizer, best_valid_loss)
+                    #save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    
+    #save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    print('Finished Training!')
+
+
+model = LSTM().to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+train(model=model, optimizer=optimizer, num_epochs=10)
