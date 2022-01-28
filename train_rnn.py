d927ee60ba34174c806dc1ac527ee2616f3f3bdf
codespace <codespace@codespaces_01f164>
2022-01-28 07:09:19 +0000

    flor.shadow.alexis@kaggle-nlp-disasters-rnn::2022-01-28T07:08:18.json

--- train_rnn.py
+++ train_rnn.py
@@ -23,7 +23,7 @@ fields = [('words', text_field), ('target', label_field)]
 fields_test = [('words', text_field)]
 
 train, valid = TabularDataset.splits(path = 'data', train='train_rnn.csv', validation='valid_rnn.csv', format='CSV', fields=fields, skip_header=True)
-test = TabularDataset.splits(path = 'data', test = 'test_rnn.csv', format='CSV', fields=fields_test, skip_header=True)
+test = TabularDataset(path = 'data/test_rnn.csv', format='CSV', fields=fields_test, skip_header=True)
 
 train_iter = BucketIterator(train, batch_size=200, sort_key=lambda x: len(x.words), device = device, sort=True, sort_within_batch=True)
 valid_iter = BucketIterator(valid, batch_size=200, sort_key=lambda x: len(x.words), device = device, sort=True, sort_within_batch=True)
