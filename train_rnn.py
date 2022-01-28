f9e8e8b8d4657f578ccb08ece0d26b6e36c90f03
codespace <codespace@codespaces_01f164>
2022-01-28 06:48:02 +0000

    flor.shadow.alexis@kaggle-nlp-disasters-rnn::2022-01-28T06:46:56.json

--- train_rnn.py
+++ train_rnn.py
@@ -65,53 +65,6 @@ class LSTM(nn.Module):
 
         return text_out
 
-# helper functions for check point 
-def save_checkpoint(save_path, model, optimizer, valid_loss):
-
-    if save_path == None:
-        return
-    
-    state_dict = {'model_state_dict': model.state_dict(),
-                  'optimizer_state_dict': optimizer.state_dict(),
-                  'valid_loss': valid_loss}
-    
-    torch.save(state_dict, save_path)
-    print(f'Model saved to ==> {save_path}')
-
-def load_checkpoint(load_path, model, optimizer):
-
-    if load_path==None:
-        return
-    
-    state_dict = torch.load(load_path, map_location=device)
-    print(f'Model loaded from <== {load_path}')
-    
-    model.load_state_dict(state_dict['model_state_dict'])
-    optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-    
-    return state_dict['valid_loss']
-
-def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
-
-    if save_path == None:
-        return
-    
-    state_dict = {'train_loss_list': train_loss_list,
-                  'valid_loss_list': valid_loss_list,
-                  'global_steps_list': global_steps_list}
-    
-    torch.save(state_dict, save_path)
-    print(f'Model saved to ==> {save_path}')
-
-def load_metrics(load_path):
-
-    if load_path==None:
-        return
-    
-    state_dict = torch.load(load_path, map_location=device)
-    print(f'Model loaded from <== {load_path}')
-    
-    return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
 
 # training 
 def train(model, optimizer, criterion = nn.BCELoss(), train_loader = train_iter, valid_loader = valid_iter, num_epochs = 5, eval_every = len(train_iter) // 2, file_path = 'training_process', best_valid_loss = float("Inf")):  
