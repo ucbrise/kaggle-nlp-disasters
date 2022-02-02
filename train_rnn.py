d87248ff56fa060c939b7efa693b96dc8317e905
Rolando Garcia <rogarcia@berkeley.edu>
2022-02-02 02:37:56 +0000

    flor.shadow.alexis@kaggle-nlp-disasters-rnn::2022-02-02T02:37:00.json

--- train_rnn.py
+++ train_rnn.py
@@ -212,7 +212,7 @@ def train(
 
 
 model = LSTM(11).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.005)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 pred = train(model=model, optimizer=optimizer, num_epochs=160)
 # print(pred)
 # print(len(pred))
