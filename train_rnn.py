406d9b1bae796fdd82f18c52a54598092371fabb
codespace <codespace@codespaces_ff6005>
2022-02-02 00:27:44 +0000

    flor.shadow.alexis@kaggle-nlp-disasters-rnn::2022-02-02T00:25:03.json

--- train_rnn.py
+++ train_rnn.py
@@ -205,7 +205,7 @@ def train(
     return y_pred
 
 
-model = LSTM(16).to(device)
+model = LSTM(13).to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 pred = train(model=model, optimizer=optimizer, num_epochs=160)
 print(pred)
