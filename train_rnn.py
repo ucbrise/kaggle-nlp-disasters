d9973057cb00a470ab29763679fd8d7f84eec1b0
Rolando Garcia <rogarcia@berkeley.edu>
2022-02-10 03:25:46 +0000

    flor.shadow@kaggle-nlp-disasters-rnn::2022-02-10T03:25:23.json

--- train_rnn.py
+++ train_rnn.py
@@ -216,7 +216,7 @@ clr_scheduler = CLR_Scheduler(
     optimizer,
     net_steps=(len(train_iter) * EPOCHS),
     min_lr=MIN_LR,
-    max_lr=12.0,
+    max_lr=16.0,
     tail_frac=0.0,
 )
 pred = train(model=model, optimizer=optimizer, num_epochs=EPOCHS)
