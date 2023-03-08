d8682423f2686bdc6067eb3fd6cde6d5fcdfb58a
Rolando Garcia <rogarcia@berkeley.edu>
2023-03-08 19:37:05 +0000

    saving

--- train_rnn.py
+++ train_rnn.py
@@ -1,188 +1,97 @@
-from multiprocessing import set_start_method
-import flor
-from flor import MTK as Flor
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from torchtext.legacy.data import BucketIterator, Field, TabularDataset
-from utils import CLR_Scheduler
-import time
+import torchvision
+import torchvision.transforms as transforms
 
-start_time = time.time()
-try:
-    set_start_method("spawn")
-except RuntimeError:
-    pass
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+import flor
+from flor import MTK as Flor
 
-label_field = Field(
-    sequential=False, use_vocab=False, batch_first=True, dtype=torch.float
+from models import RNN
+
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+flor.log("device", str(device))
+
+# Hyper-parameters
+sequence_length = 28
+input_size = 28
+hidden_size = 128
+num_layers = 2
+num_classes = 10
+batch_size = 100
+num_epochs = 2
+learning_rate = 0.01
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(
+    root="../../data/", train=True, transform=transforms.ToTensor(), download=True
 )
-text_field = Field(tokenize="spacy", lower=True, include_lengths=True, batch_first=True)
-fields = [("words", text_field), ("target", label_field)]
-fields_test = [("words", text_field)]
-(train, valid) = TabularDataset.splits(
-    path="data",
-    train="train_rnn.csv",
-    validation="valid_rnn.csv",
-    format="CSV",
-    fields=fields,
-    skip_header=True,
+flor.log("train_dataset", str(type(train_dataset)))
+
+test_dataset = torchvision.datasets.MNIST(
+    root="../../data/", train=False, transform=transforms.ToTensor()
 )
-test = TabularDataset(
-    path="data/test_rnn.csv", format="CSV", fields=fields_test, skip_header=True
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(  # type: ignore
+    dataset=train_dataset, batch_size=batch_size, shuffle=True
 )
-BATCH_SIZE = 64
-train_iter = BucketIterator(
-    train,
-    batch_size=BATCH_SIZE,
-    sort_key=lambda x: len(x.words),
-    device=device,
-    sort=True,
-    sort_within_batch=True,
+
+test_loader = torch.utils.data.DataLoader(  # type: ignore
+    dataset=test_dataset, batch_size=batch_size, shuffle=False
 )
-valid_iter = BucketIterator(
-    valid,
-    batch_size=BATCH_SIZE,
-    sort_key=lambda x: len(x.words),
-    device=device,
-    sort=True,
-    sort_within_batch=True,
-)
-test_iter = BucketIterator(
-    test,
-    batch_size=BATCH_SIZE,
-    sort_key=lambda x: len(x.words),
-    device=device,
-    sort=True,
-    sort_within_batch=True,
-)
-text_field.build_vocab(train, min_freq=5)
+
+# Recurrent neural network (many-to-one)
+model = RNN(input_size, hidden_size, num_layers, num_classes, device).to(device)
 
 
-class LSTM(nn.Module):
-    def __init__(self, dimension=128):
-        super(LSTM, self).__init__()
-        self.embedding = nn.Embedding(len(text_field.vocab), dimension)
-        self.lstm = nn.LSTM(
-            input_size=dimension,
-            hidden_size=dimension,
-            num_layers=1,
-            batch_first=True,
-            bidirectional=True,
-        )
-        self.drop = nn.Dropout(p=0.85)
-        self.dimension = dimension
-        self.fc = nn.Linear(2 * dimension, 1)
-        self.relu = nn.ReLU()
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+Flor.checkpoints(model, optimizer)
 
-    def forward(self, text, text_len):
-        text_emb = self.relu(self.embedding(text))
-        packed_input = pack_padded_sequence(
-            text_emb, text_len, batch_first=True, enforce_sorted=False
-        )
-        (packed_output, _) = self.lstm(packed_input)
-        (output, _) = pad_packed_sequence(packed_output, batch_first=True)
-        out_forward = output[range(len(output)), text_len - 1, : self.dimension]
-        out_reverse = output[:, 0, self.dimension :]
-        out_reduced = torch.cat((out_forward, out_reverse), 1)
-        text_fea = out_reduced
-        text_fea = self.fc(self.drop(text_fea))
-        text_fea = torch.squeeze(text_fea, 1)
-        text_out = torch.sigmoid(text_fea)
-        return text_out
+# Train the model
+total_step = len(train_loader)
+for epoch in Flor.loop(range(num_epochs)):
+    for i, (images, labels) in Flor.loop(enumerate(train_loader)):  # type: ignore
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
 
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
 
-def train(
-    model,
-    optimizer,
-    criterion=nn.BCELoss(),
-    train_loader=train_iter,
-    valid_loader=valid_iter,
-    test_loader=test_iter,
-    num_epochs=len(train_iter) // 10,
-    eval_every=4,
-    file_path="training_process",
-    best_valid_loss=float("Inf"),
-):
-    running_loss = 0.0
-    valid_running_loss = 0.0
-    global_step = 0
-    train_loss_list = []
-    valid_loss_list = []
-    global_steps_list = []
-    best_loss = float("inf")
-    model.train()
-    Flor.checkpoints(model, optimizer)
-    for epoch in Flor.loop(range(num_epochs)):
-        for (((words, words_len), labels), _) in Flor.loop(train_loader):
-            labels = labels.to(device)
-            words = words.to(device)
-            words_len = words_len.detach().cpu()
-            output = model(words, words_len)
-            loss = criterion(output, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            global_step += 1
-            if global_step % eval_every == 0:
-                model.eval()
-                with torch.no_grad():
-                    for (((words, words_len), labels), _) in valid_loader:
-                        labels = labels.to(device)
-                        words = words.to(device)
-                        words_len = words_len.detach().cpu()
-                        output = model(words, words_len)
-                        loss = criterion(output, labels)
-                        valid_running_loss += float(loss.item())
-                average_train_loss = running_loss / eval_every
-                average_valid_loss = valid_running_loss / len(valid_loader)
-                if average_valid_loss < best_loss:
-                    best_loss = average_valid_loss
-                    torch.save(model.state_dict(), "best-model.pt")
-                train_loss_list.append(average_train_loss)
-                valid_loss_list.append(average_valid_loss)
-                global_steps_list.append(global_step)
-                running_loss = 0.0
-                valid_running_loss = 0.0
-                model.train()
-                lr_scheduler.step()
-                print(
-                    "Epoch [{}/{}], LR: {:.3f}, Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}".format(
-                        epoch + 1,
-                        num_epochs,
-                        optimizer.param_groups[0]["lr"],
-                        global_step,
-                        num_epochs * len(train_loader),
-                        average_train_loss,
-                        average_valid_loss,
-                    )
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(
+                "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                    epoch + 1,
+                    num_epochs,
+                    i + 1,
+                    total_step,
+                    flor.log("loss", float(loss.item())),
                 )
-                flor.log("avg_train_loss", average_train_loss)
-                flor.log("average_valid_loss", average_valid_loss)
-    y_pred = []
-    model.eval()
-    with torch.no_grad():
-        for ((words, words_len), _) in test_loader:
-            words = words.to(device)
-            words_len = words_len.detach().cpu()
-            output = model(words, words_len)
-            output = (output > 0.5).int()
-            y_pred.extend(output.tolist())
-    print("Finished Training!")
-    return y_pred
+            )
 
+# Test the model
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-EPOCHS = 20
-MIN_LR = 0.0001
-model = LSTM(8).to(device)
-optimizer = optim.SGD(model.parameters(), lr=MIN_LR)
-lr_scheduler = CLR_Scheduler(optimizer, len(train_iter) * EPOCHS, MIN_LR, max_lr=2.0)
-pred = train(model=model, optimizer=optimizer, num_epochs=EPOCHS)
+    print(
+        "Test Accuracy of the model on the 10000 test images: {} %".format(
+            flor.log("accuracy", 100 * correct / total)
+        )
+    )
---
+++ train_rnn_backup.py
@@ -0,0 +1,188 @@
+from multiprocessing import set_start_method
+import flor
+from flor import MTK as Flor
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torchtext.legacy.data import BucketIterator, Field, TabularDataset
+from utils import CLR_Scheduler
+import time
+
+start_time = time.time()
+try:
+    set_start_method("spawn")
+except RuntimeError:
+    pass
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+label_field = Field(
+    sequential=False, use_vocab=False, batch_first=True, dtype=torch.float
+)
+text_field = Field(tokenize="spacy", lower=True, include_lengths=True, batch_first=True)
+fields = [("words", text_field), ("target", label_field)]
+fields_test = [("words", text_field)]
+(train, valid) = TabularDataset.splits(
+    path="data",
+    train="train_rnn.csv",
+    validation="valid_rnn.csv",
+    format="CSV",
+    fields=fields,
+    skip_header=True,
+)
+test = TabularDataset(
+    path="data/test_rnn.csv", format="CSV", fields=fields_test, skip_header=True
+)
+BATCH_SIZE = 64
+train_iter = BucketIterator(
+    train,
+    batch_size=BATCH_SIZE,
+    sort_key=lambda x: len(x.words),
+    device=device,
+    sort=True,
+    sort_within_batch=True,
+)
+valid_iter = BucketIterator(
+    valid,
+    batch_size=BATCH_SIZE,
+    sort_key=lambda x: len(x.words),
+    device=device,
+    sort=True,
+    sort_within_batch=True,
+)
+test_iter = BucketIterator(
+    test,
+    batch_size=BATCH_SIZE,
+    sort_key=lambda x: len(x.words),
+    device=device,
+    sort=True,
+    sort_within_batch=True,
+)
+text_field.build_vocab(train, min_freq=5)
+
+
+class LSTM(nn.Module):
+    def __init__(self, dimension=128):
+        super(LSTM, self).__init__()
+        self.embedding = nn.Embedding(len(text_field.vocab), dimension)
+        self.lstm = nn.LSTM(
+            input_size=dimension,
+            hidden_size=dimension,
+            num_layers=1,
+            batch_first=True,
+            bidirectional=True,
+        )
+        self.drop = nn.Dropout(p=0.85)
+        self.dimension = dimension
+        self.fc = nn.Linear(2 * dimension, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, text, text_len):
+        text_emb = self.relu(self.embedding(text))
+        packed_input = pack_padded_sequence(
+            text_emb, text_len, batch_first=True, enforce_sorted=False
+        )
+        (packed_output, _) = self.lstm(packed_input)
+        (output, _) = pad_packed_sequence(packed_output, batch_first=True)
+        out_forward = output[range(len(output)), text_len - 1, : self.dimension]
+        out_reverse = output[:, 0, self.dimension :]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+        text_fea = out_reduced
+        text_fea = self.fc(self.drop(text_fea))
+        text_fea = torch.squeeze(text_fea, 1)
+        text_out = torch.sigmoid(text_fea)
+        return text_out
+
+
+def train(
+    model,
+    optimizer,
+    criterion=nn.BCELoss(),
+    train_loader=train_iter,
+    valid_loader=valid_iter,
+    test_loader=test_iter,
+    num_epochs=len(train_iter) // 10,
+    eval_every=4,
+    file_path="training_process",
+    best_valid_loss=float("Inf"),
+):
+    running_loss = 0.0
+    valid_running_loss = 0.0
+    global_step = 0
+    train_loss_list = []
+    valid_loss_list = []
+    global_steps_list = []
+    best_loss = float("inf")
+    model.train()
+    Flor.checkpoints(model, optimizer)
+    for epoch in Flor.loop(range(num_epochs)):
+        for (((words, words_len), labels), _) in Flor.loop(train_loader):
+            labels = labels.to(device)
+            words = words.to(device)
+            words_len = words_len.detach().cpu()
+            output = model(words, words_len)
+            loss = criterion(output, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            global_step += 1
+            if global_step % eval_every == 0:
+                model.eval()
+                with torch.no_grad():
+                    for (((words, words_len), labels), _) in valid_loader:
+                        labels = labels.to(device)
+                        words = words.to(device)
+                        words_len = words_len.detach().cpu()
+                        output = model(words, words_len)
+                        loss = criterion(output, labels)
+                        valid_running_loss += float(loss.item())
+                average_train_loss = running_loss / eval_every
+                average_valid_loss = valid_running_loss / len(valid_loader)
+                if average_valid_loss < best_loss:
+                    best_loss = average_valid_loss
+                    torch.save(model.state_dict(), "best-model.pt")
+                train_loss_list.append(average_train_loss)
+                valid_loss_list.append(average_valid_loss)
+                global_steps_list.append(global_step)
+                running_loss = 0.0
+                valid_running_loss = 0.0
+                model.train()
+                lr_scheduler.step()
+                print(
+                    "Epoch [{}/{}], LR: {:.3f}, Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}".format(
+                        epoch + 1,
+                        num_epochs,
+                        optimizer.param_groups[0]["lr"],
+                        global_step,
+                        num_epochs * len(train_loader),
+                        average_train_loss,
+                        average_valid_loss,
+                    )
+                )
+                flor.log("avg_train_loss", average_train_loss)
+                flor.log("average_valid_loss", average_valid_loss)
+    y_pred = []
+    model.eval()
+    with torch.no_grad():
+        for ((words, words_len), _) in test_loader:
+            words = words.to(device)
+            words_len = words_len.detach().cpu()
+            output = model(words, words_len)
+            output = (output > 0.5).int()
+            y_pred.extend(output.tolist())
+    print("Finished Training!")
+    return y_pred
+
+
+EPOCHS = 20
+MIN_LR = 0.0001
+model = LSTM(8).to(device)
+optimizer = optim.SGD(model.parameters(), lr=MIN_LR)
+lr_scheduler = CLR_Scheduler(optimizer, len(train_iter) * EPOCHS, MIN_LR, max_lr=2.0)
+pred = train(model=model, optimizer=optimizer, num_epochs=EPOCHS)
